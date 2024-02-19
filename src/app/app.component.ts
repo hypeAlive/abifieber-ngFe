82a526c724b26a8900145c236771dcca9f3e6a8c
Alive <nicolasfr002@gmail.com>
2024-02-19 14:02:51 +0100

    updated project using ng(x)-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, TemplateRef} from '@angular/core';
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
+import {BsDropdownModule} from "ngx-bootstrap/dropdown";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    BsDropdownModule
+  ],
+  standalone: true
 })
 export class AppComponent {
-  title = 'abifieber-ngFe';
+  protected title = 'Bootstrap App';
+
+  protected modalRef: BsModalRef | undefined;
+
+  constructor(private modalService: BsModalService) {}
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {ModalModule} from "ngx-bootstrap/modal";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
+      ModalModule.forRoot()
+    )
+  ]
 };
