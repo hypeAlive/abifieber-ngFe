8139bbcbd00bc758c54e42567cd378d9d63531e4
Alive <nicolasfr002@gmail.com>
2024-02-19 15:04:37 +0100

    updates types

--- src/app/shared/order.types.ts
+++ src/app/shared/order.types.ts
@@ -2,11 +2,14 @@ import {Product} from "./product.types";
 import {User} from "./user.types";
 
 export interface Order {
+  id: number,
   user: User,
   orderPositions: OrderPosition[]
 }
 
 export interface OrderPosition {
+  id: number,
+  order: Order,
   product: Product,
   quantity: number
 }
--- src/app/shared/product.types.ts
+++ src/app/shared/product.types.ts
@@ -1,6 +1,7 @@
 export interface Product {
   id: number;
-  name: string;
+  title: string;
+  image: string;
   description: string;
   price: number;
 }
